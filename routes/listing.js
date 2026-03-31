a9c12e749ad8e32419573e89bf35f92d12661208
PrinceKicker <erprincepatel12@gmail.com>
2026-03-31 19:26:17 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner , validateListing} = require("../middleware.js");
+
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
+router
+   .route("/")
+   .get(wrapAsync(listingController.index))//Index Route - to show all listings
+   .post(//Create Route - to create new listing
+    isLoggedIn, 
+    upload.single('listing[image]'),
+    validateListing, 
+    wrapAsync(listingController.createListing)
+);
+    
+
+//New Route - to show form to create new listing
+router.get("/new", isLoggedIn,listingController.renderNewForm );
+
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))//Show Route - to show details of one listing
+    .put(isLoggedIn,isOwner ,upload.single('listing[image]'),validateListing, wrapAsync(listingController.updateListing))//Update Route
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));//Delete Route 
+
+
+//Edit Route - to show form to edit a listing
+router.get("/:id/edit", isLoggedIn,isOwner ,wrapAsync(listingController.renderEditForm));
+
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const exprees = require("express");
+const router = exprees.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+const {saveRedirectURL} = require("../middleware.js");
+
+const userController = require("../controllers/users.js");
+
+router.route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
+
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post( saveRedirectURL,
+    passport.authenticate("local", {
+        failureRedirect:'/login',
+        failureFlash:true,
+    }), 
+    userController.login
+);
+
+router.get("/logout",userController.logout);
+
+module.exports = router;
